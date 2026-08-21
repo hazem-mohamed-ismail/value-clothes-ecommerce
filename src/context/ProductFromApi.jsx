1eb3cb8511aba0f46be833518a43bdbb826d2984
hazem-mohamed-ismail <hazemmelaskry@gmail.com>
2026-08-22 00:33:48 +0300

    Update ProductFromApi.jsx

--- src/context/ProductFromApi.jsx
+++ src/context/ProductFromApi.jsx
@@ -18,7 +18,7 @@ function ProductFromApi({ children }) {
         console.error("Error fetching products:", error);
         setError(true);
       } finally {
-          document.body.scrollTop = 0;
+         
         setLoading(false);
       }
     }
